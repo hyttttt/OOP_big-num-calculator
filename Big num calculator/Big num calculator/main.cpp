a4c045773dd81ca6b3d4cb6d39839988078b7eda
gary <garyandhouse@.gmail.com>
2022-05-02 00:02:45 +0800

    重構

--- Big num calculator/Big num calculator/main.cpp
+++ Big num calculator/Big num calculator/main.cpp
@@ -2,38 +2,97 @@
 #include <sstream>
 #include <stack>
 #include <string>
+#include <vector>
+#include <map>
 
 using namespace std;
+class VariableInfo {
+	//vType 0為Integer 1為Decimal 
+public: 
+	int vType;
+	//
+    string value;
+};
 
 int priority(char op) {
-
+	return 0;
 }
 
-string infixToPostfix(string infix) {
-
+vector<string> infixToPostfix(vector<string> infix) {
+	vector<string>output;
+	return output;
 }
 
-string calculate(string postfix) {
-
+string calculate(vector<string> postfix) {
+	return"";
 }
 
 int main() {
 	string input;
+
+	
+	//儲存變數資訊
+	map < string, VariableInfo > variable= map < string, VariableInfo >();
+	
 	while (true)
 	{
 		getline(cin, input);
+		stringstream ss;
+		ss.str("");
+		ss.clear();
+
+		string temp;
+		ss << input;
+
+		ss >> temp;
 
 		//stop input
-		if (input == "exit")	break;
+		if (temp == "exit")	break;
 
 		//set variable
-		else if ((input[0] == 's' && input[1] == 'e') && input[2] == 't') {
+		else if (temp=="set"|| temp == "Set"|| temp == "SET") {
 
+			VariableInfo variableInfo = VariableInfo();
+			string name;
+
+			ss >> temp;
+			//get name
+			ss >> name;
+			ss >> variableInfo.value;
+			if (temp == "int" || temp == "Integer") {
+				//get type
+				variableInfo.vType = 0;
+				//儲存變數資訊
+				if (variable.find(name) != variable.end()) {
+					variable[name] = variableInfo;
+				}
+				else {
+					variable.insert(pair<string, VariableInfo>(name, variableInfo));
+				};
+			}
+			else if (temp == "float" || temp == "Decimal") {
+				//get type
+				variableInfo.vType = 1;
+				//儲存變數資訊
+				if (variable.find(name) != variable.end()) {
+					variable[name] = variableInfo;
+				}
+				else {
+					variable.insert(pair<string, VariableInfo>(name, variableInfo));
+				};
+			}
 		}
 
 		//calculation
 		else {
-			string postfix = infixToPostfix(input);
+			vector<string>stemp;
+			stemp.push_back(temp);
+			while (!ss) {
+				temp = "";
+				ss >> temp;
+				stemp.push_back(temp);
+			}
+			vector <string> postfix = infixToPostfix(stemp);
 			string result = calculate(postfix);
 			cout << result << endl;
 		}
