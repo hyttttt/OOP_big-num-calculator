8a12d2db8d2571507294d8aabb77a75ee287d10d
gary <garyandhouse@.gmail.com>
2022-05-03 21:41:59 +0800

    change

--- Big num calculator/Big num calculator/main.cpp
+++ Big num calculator/Big num calculator/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include"Decimal.h"
 
 using namespace std;
 class VariableInfo {
@@ -12,27 +13,79 @@ public:
 	int vType;
 	//
     string value;
+	
 };
 
-int priority(char op) {
+
+//儲存變數資訊
+map < string, VariableInfo > variable = map < string, VariableInfo >();
+
+int priority(string op) {
+	if (op == "+"  || op == "-") {
+		return 1;
+	}
+	if (op == "*" || op == "/") {
+		return 2;
+	}
 	return 0;
 }
 
 vector<string> infixToPostfix(vector<string> infix) {
+	vector<string>istack;
 	vector<string>output;
+	istack.clear();
+	output.clear();
+
+	
+
+	for (int i = 0; i < infix.size(); i++) {
+		if (infix[i] == "(") {
+			istack.push_back(infix[i]);
+		}
+		else if (infix[i]=="+" || infix[i] == "-" || infix[i] == "*" || infix[i] == "/") {
+			if (istack.size()==0 || istack[istack.size() - 1] == "(") {
+				istack.push_back(infix[i]);
+			}
+			else if (priority(istack[istack.size() - 1]) < priority(infix[i])) {
+				istack.push_back(infix[i]);
+			}
+			else {
+				output.push_back(istack[istack.size() - 1]);
+				istack.pop_back();
+				istack.push_back(infix[i]);
+			}
+		}
+		else if (infix[i] == ")") {
+			while (istack[istack.size() - 1] != "(") {
+				output.push_back(istack[istack.size() - 1]);
+				istack.pop_back();
+			}
+			istack.pop_back();
+		}
+		else {
+			output.push_back(infix[i]);
+		}
+	}
+	while (istack.size() != 0) {
+		output.push_back(istack[istack.size() - 1]);
+		istack.pop_back();
+	}
 	return output;
 }
 
 string calculate(vector<string> postfix) {
-	return"";
+	vector<string>istack;
+	for (int i = 0; i < postfix.size(); i++) {
+		
+	}
+	return variable[postfix[0]].value;
 }
 
 int main() {
 	string input;
 
 	
-	//儲存變數資訊
-	map < string, VariableInfo > variable= map < string, VariableInfo >();
+	
 	
 	while (true)
 	{
@@ -54,40 +107,37 @@ int main() {
 
 			VariableInfo variableInfo = VariableInfo();
 			string name;
-
+			string garbageMessage;
 			ss >> temp;
 			//get name
 			ss >> name;
+			//清除 =
+			ss >> garbageMessage;
 			ss >> variableInfo.value;
 			if (temp == "int" || temp == "Integer") {
 				//get type
 				variableInfo.vType = 0;
-				//儲存變數資訊
-				if (variable.find(name) != variable.end()) {
-					variable[name] = variableInfo;
-				}
-				else {
-					variable.insert(pair<string, VariableInfo>(name, variableInfo));
-				};
+				
 			}
 			else if (temp == "float" || temp == "Decimal") {
 				//get type
 				variableInfo.vType = 1;
-				//儲存變數資訊
-				if (variable.find(name) != variable.end()) {
-					variable[name] = variableInfo;
-				}
-				else {
-					variable.insert(pair<string, VariableInfo>(name, variableInfo));
-				};
+				
 			}
+			//儲存變數資訊
+			if (variable.find(name) != variable.end()) {
+				variable[name] = variableInfo;
+			}
+			else {
+				variable.insert(pair<string, VariableInfo>(name, variableInfo));
+			};
 		}
 
 		//calculation
 		else {
 			vector<string>stemp;
 			stemp.push_back(temp);
-			while (!ss) {
+			while (ss) {
 				temp = "";
 				ss >> temp;
 				stemp.push_back(temp);
